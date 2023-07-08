d9c4727c4f2b92fbdebeb47018b73de814792134
Sachin <sachinlakshan04@gmail.com>
2023-07-09 00:57:46 +0530

    wip - update design

--- client/src/components/TransactionsGrid.tsx
+++ client/src/components/TransactionsGrid.tsx
@@ -2,7 +2,18 @@ import { Box, IconButton, Tooltip } from "@mui/material";
 import { Transaction } from "../models/Transaction";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { Delete, Edit } from "@mui/icons-material";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import dayjs from "dayjs";
+import { useState } from "react";
 
 interface Props {
   transactions: Transaction[];
@@ -10,6 +21,8 @@ interface Props {
 }
 
 const TransactionsGrid = ({ transactions, onDelete }: Props) => {
+  const [open, setOpen] = useState<boolean>(false);
+
   const columns: GridColDef[] = [
     {
       field: "name",
@@ -43,21 +56,23 @@ const TransactionsGrid = ({ transactions, onDelete }: Props) => {
       flex: 1,
       renderCell: (params) => {
         return (
-          <Box>
-            <Tooltip title="Edit the transaction">
-              <IconButton sx={{ color: "blue" }}>
-                <Edit />
-              </IconButton>
-            </Tooltip>
-            <Tooltip title="Delete the transaction">
-              <IconButton
-                sx={{ color: "red" }}
-                onClick={() => removeTransaction(params.row._id)}
-              >
-                <Delete />
-              </IconButton>
-            </Tooltip>
-          </Box>
+          <div>
+            <Box>
+              <Tooltip title="Edit the transaction">
+                <IconButton sx={{ color: "blue" }} onClick={handleClickOpen}>
+                  <Edit />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Delete the transaction">
+                <IconButton
+                  sx={{ color: "red" }}
+                  onClick={() => removeTransaction(params.row._id)}
+                >
+                  <Delete />
+                </IconButton>
+              </Tooltip>
+            </Box>
+          </div>
         );
       },
     },
@@ -71,6 +86,14 @@ const TransactionsGrid = ({ transactions, onDelete }: Props) => {
 
   const formatDate = (date: string) => {
     return dayjs(date).format("LL");
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   return (
@@ -93,6 +116,46 @@ const TransactionsGrid = ({ transactions, onDelete }: Props) => {
             disableRowSelectionOnClick
             // checkboxSelection
           />
+          <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Edit Transaction</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                To Edit this transaction, please enter your new transaction
+                details here. We will send updates immediately.
+              </DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Transaction Name"
+                type="text"
+                fullWidth
+                variant="outlined"
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Transaction Amount"
+                type="number"
+                fullWidth
+                variant="outlined"
+              />
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  label="Transaction Date"
+                  defaultValue={dayjs("2022-04-17")}
+                  slotProps={{
+                    textField: { fullWidth: true, margin: "dense" },
+                  }}
+                />
+              </LocalizationProvider>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button onClick={handleClose}>Save</Button>
+            </DialogActions>
+          </Dialog>
         </div>
       </div>
     </>

950a1a2d76fe77251a399085edf9cf4522a43283
Sachin <sachinlakshan04@gmail.com>
2023-07-11 17:26:13 +0530

    register user part 2

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,32 @@
 import { Router } from "express";
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
 
 const router = Router();
 
 router.post("/signup", async (req, res) => {
-  res.json({ message: "User is created successfully" });
+  const { email, firstName, lastName, password } = req.body;
+
+  //1. check if user exist with the same username
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(409).json({ message: "Email address already exists" });
+    return;
+  }
+
+  //2. Hash the password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  //3. Store the user in DB
+  const user = new User({
+    email,
+    firstName,
+    lastName,
+    password: hashedPassword,
+  });
+  await user.save();
+
+  res.status(201).json({ message: "User is created successfully" });
 });
 
 export default router;
